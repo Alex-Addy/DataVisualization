cdd990d5ae173cfd3269a861552902127364f7ba
Alexander Addy <alex.addy@gmail.com>
2014-10-18 13:25:33 -0400

    Project Creation.

---
+++ Data Visualization/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Data_Visualization
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
